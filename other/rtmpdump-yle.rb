72f9ad0138efd4bdbfdcd234e74ee13a29e80f70
Ville Lautanala <lautis@gmail.com>
2011-06-29 23:17:36 +0300

    Update rtmpdump-yle to 1.4.2

--- other/rtmpdump-yle.rb
+++ other/rtmpdump-yle.rb
@@ -2,11 +2,13 @@ require 'formula'
 
 class RtmpdumpYle < Formula
   depends_on 'json-c'
-  url 'http://users.tkk.fi/~aajanki/rtmpdump-yle/rtmpdump-yle-1.4.1.tar.gz'
+  url 'http://users.tkk.fi/~aajanki/rtmpdump-yle/rtmpdump-yle-1.4.2.tar.gz'
   homepage 'http://users.tkk.fi/~aajanki/rtmpdump-yle/index.html'
-  md5 '847b33a19161390d2dc568255af244d1'
+  md5 '5bdbf955d3b4ddb06eaa13b326d9d80d'
 
   def install
+    ENV["INC"] = "-I#{HOMEBREW_PREFIX}/include/json"
+
     system "make", "SYS=darwin"
     system "make", "install", "SYS=darwin", "prefix=#{prefix}", "mandir=#{man}"
   end
